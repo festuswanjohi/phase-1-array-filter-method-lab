25600fb94210d68b78f9ad641058c917e364ed4f
festus wanjohi <strategic.policyafrica@gmail.com>
2022-08-18 23:04:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+function findMatching(array, driver) {
+  return array.filter(function (mydriver) {
+    return mydriver.toLowerCase() === driver.toLowerCase();
+  });
+}
+function fuzzyMatch(array, driver) {
+  return array.filter(function (mydriver) {
+    return (
+      mydriver.toLowerCase().includes(driver.toLowerCase(), 0) &&
+      !mydriver.toLowerCase().includes(driver.toLowerCase(), 1)
+    );
+  });
+}
+function matchName(array, driver) {
+  return array.filter((mydriver) => mydriver.name === driver);
+}
